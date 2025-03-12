5343bec17069880ed119a94964a98aa8823a0473
umesh <umesh37kr@gmail.com>
2025-03-12 17:10:56 +0530

    add inout ques

--- input-output.js
+++ input-output.js
@@ -1,5 +1,5 @@
 // ****************************
-// 1.
+// 1. what will be the output
 // ****************************
 // let age = 10;
 // const obj = {
@@ -12,14 +12,42 @@
 // obj.firstFn();
 
 // ****************************
-// 2.
+// 2. what will be the output
 // ****************************
 // console.log(typeof undefined);
 // console.log(typeof null);
 
 // ****************************
-// 3.
+// 3. what will be the output
 // ****************************
 // const a = null;
 // const value = a || "Default value";
 // console.log(value);
+
+// ****************************
+// 4. what will be the output
+// ****************************
+// const student = {
+//   name: "John",
+//   courses: {
+//     math: {
+//       grade: "A",
+//     },
+//   },
+// };
+// const historyGrade = student.courses.history.grade;
+// console.log(historyGrade);
+
+// ****************************
+// 5. what can you do to above code to run without error
+// ****************************
+// const student = {
+//   name: "John",
+//   courses: {
+//     math: {
+//       grade: "A",
+//     },
+//   },
+// };
+// const historyGrade = student.courses.history?.grade;
+// console.log(historyGrade);
